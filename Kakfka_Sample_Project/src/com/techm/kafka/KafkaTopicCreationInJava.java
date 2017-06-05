558841a45a46ca5dc044419d704b33c05c618d7d
venkat <venkat@DESKTOP-SS5HA7D>
2017-06-05 22:19:29 +0530

    this is second commit

--- Kakfka_Sample_Project/src/com/techm/kafka/KafkaTopicCreationInJava.java
+++ Kakfka_Sample_Project/src/com/techm/kafka/KafkaTopicCreationInJava.java
@@ -1,16 +1,17 @@
 package com.techm.kafka;
 
+import java.util.List;
 import java.util.Properties;
 
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
 import org.apache.kafka.common.requests.MetadataResponse.TopicMetadata;
+import org.apache.zookeeper.ZooKeeper;
 
 import kafka.admin.AdminUtils;
-import kafka.admin.RackAwareMode;
 import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
-
+////hello this is topic creation.
 public class KafkaTopicCreationInJava
 {
     public static void main(String[] args) throws Exception {
@@ -43,4 +44,16 @@ public class KafkaTopicCreationInJava
             }
         }
     }
+    
+    public static void printBrokerHosts() throws Exception {
+        ZooKeeper zk = new ZooKeeper("localhost:2181", 10000, null);
+        List<String> ids = zk.getChildren("/brokers/ids", false);
+        for (String id : ids) {
+            String brokerInfo = new String(zk.getData("/brokers/ids/" + id, false, null));
+            System.out.println(id + ": " + brokerInfo);
+        }
+    }
+//    1: {"jmx_port":-1,"timestamp":"1428512949385","host":"192.168.0.11","version":1,"port":9093}
+//    2: {"jmx_port":-1,"timestamp":"1428512955512","host":"192.168.0.11","version":1,"port":9094}
+//    3: {"jmx_port":-1,"timestamp":"1428512961043","host":"192.168.0.11","version":1,"port":9095
 }
